c29c5dd1dba04f59862dd47c38f428e088539e91
NIdjoabi <if24b103@technikum-wien.at>
2026-03-25 01:46:34 +0100

    Import gefixt

--- src/app/create-tour-page/create-tour-page.ts
+++ src/app/create-tour-page/create-tour-page.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Tourlist } from '../tourlist/tourlist';
 import { MapComponent } from '../map/map';
-import { Createtour } from '../createtour/createtour';
+import { CreateTourComponent } from '../tour/tour';
 
 @Component({
   selector: 'app-create-tour-page',
-  imports: [CommonModule, Tourlist, MapComponent, Createtour],
+  imports: [CommonModule, Tourlist, MapComponent, CreateTourComponent],
   templateUrl: './create-tour-page.html',
   styleUrl: './create-tour-page.css',
 })

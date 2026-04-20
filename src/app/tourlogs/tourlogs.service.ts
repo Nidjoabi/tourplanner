0493bcdcd037a24f0ad4a1caa3e03b1ea6c321ee
NIdjoabi <if24b103@technikum-wien.at>
2026-04-20 16:24:44 +0200

    api service erstellt

--- src/app/tourlogs/tourlogs.service.ts
+++ src/app/tourlogs/tourlogs.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 import { Tour } from './tourlogs.model';
+import { ApiService } from '../../services/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TourlogsService {
+
+  constructor(private apiService: ApiService) { }
+
+  getTourLogs(): Observable<Tour[]> {
+    return this.apiService.get<Tour[]>('tourlogs');
+  }
+
   editTourLog(tour: Tour): Observable<Tour> {
     return of(tour);
   }

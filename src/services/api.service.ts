e9ef1acc803eb6d9f6d33f12ba1b75a91e168568
Krystian Piotr Kedzior <krystian.kedzior515@gmail.com>
2026-06-28 19:59:07 +0200

    token handling im apiservice hinzugefügt

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 @Injectable({
@@ -10,19 +10,33 @@ export class ApiService {
 
     constructor(private http: HttpClient) {};
 
+    private getHeaders(): HttpHeaders {
+      const token = sessionStorage.getItem('token');
+      return token
+        ? new HttpHeaders({ Authorization: `Bearer ${token}` })
+        : new HttpHeaders();
+      }
+
     get<T>(endpoint: string): Observable<T> {
-        return this.http.get<T>(`${this.baseUrl}/${endpoint}`);
+        return this.http.get<T>(`${this.baseUrl}/${endpoint}`, { headers: this.getHeaders() });
     }
 
     post<T>(endpoint: string, body: unknown): Observable<T> {
-        return this.http.post<T>(`${this.baseUrl}/${endpoint}`, body);
+        return this.http.post<T>(`${this.baseUrl}/${endpoint}`, body, { headers: this.getHeaders() });
+    }
+
+    postText(endpoint: string, body: unknown): Observable<string> {
+      return this.http.post(`${this.baseUrl}/${endpoint}`, body, {
+        headers: this.getHeaders(),
+        responseType: 'text'
+      });
     }
 
     put<T>(endpoint: string, body: unknown): Observable<T> {
-        return this.http.put<T>(`${this.baseUrl}/${endpoint}`, body);
+        return this.http.put<T>(`${this.baseUrl}/${endpoint}`, body, { headers: this.getHeaders() });
     }
 
     delete<T>(endpoint: string): Observable<T> {
-        return this.http.delete<T>(`${this.baseUrl}/${endpoint}`);
+        return this.http.delete<T>(`${this.baseUrl}/${endpoint}`, { headers: this.getHeaders() });
     }
 }
